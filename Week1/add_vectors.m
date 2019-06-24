123ebc7070dfcc653a893021c2338105fab21c3a
Sheng <shengx2010@gmail.com>
2019-06-24 22:53:50 +0200

    Week 1
    Matlab Practice

---
+++ Week1/add_vectors.m
@@ -0,0 +1,13 @@
+function z = add_vectors(x, y)
+    
+    %Some examples of incorrect solutions:
+    % z = x - y;  %delete "%" to uncomment a line
+    % z = [4; 6];
+    % z = x' + y';
+    
+    %And just because this is your first time here, this is the correct solution:
+    % z = x + y; %delete "%" to uncomment this line
+    
+    %Write your own code here:
+    
+end
